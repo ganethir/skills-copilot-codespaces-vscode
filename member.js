d71277fec2437690c237175a8b9511b412364b43
ganethir <ganesh.babu4@tcs.com>
2025-03-07 05:23:20 +0000

    git commit -m "Copilot second commit"

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+    var member = {
+        name: "John Doe",
+        age: 25,
+        skills: ["JavaScript", "HTML", "CSS"],
+        address: {
+            city: "New York",
+            country: "USA"
+        }
+    };
+    return member;
+}
